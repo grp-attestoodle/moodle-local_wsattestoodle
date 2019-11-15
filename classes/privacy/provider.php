624d2ba09793a57604b96758574a39293aca7534
Leconte <marc.leconte@univ-lemans.fr>
2019-11-15 15:35:37 +0100

    Update provider.php

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace local_gdpr_deleteuserdata\privacy;
+namespace local_wsattestoodle\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
